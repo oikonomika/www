669fbeab5f91761e09463a91e5d151926fb1bc24
yuto <yuto.katsuragi@oikonomika.jp>
2018-10-04 11:28:15 +0900

    スクロールでアイコン類が表示されたときにフェードインさせる

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -20,6 +20,23 @@ function setHeaderScrollAnimation() {
   })
 }
 
+function setScrollFadein() {
+  $(window).scroll(function (){
+    $('.fadein').each(function(){
+      var elemPos = $(this).offset().top
+      var scroll = $(window).scrollTop()
+      var windowHeight = $(window).height()
+      if (scroll > elemPos - windowHeight + 200){
+        $(this).addClass('scrollin')
+      }
+      else {
+        $(this).removeClass('scrollin')
+      }
+    })
+  })
+}
+
 $(() => {
   setHeaderScrollAnimation()
+  setScrollFadein()
 })
