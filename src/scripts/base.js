743eab698eb92db64793c48ef57b39d1a49e49e2
yuto <yuto.katsuragi@oikonomika.jp>
2018-10-15 01:29:07 +0900

    少しでもスクロールしたらヘッダーをコンパクトにするように修正する

--- src/scripts/base.js
+++ src/scripts/base.js
@@ -3,7 +3,7 @@ function setHeaderScrollAnimation() {
     const headerElms = [$('header'), $('nav'), $('h1 img')]
     const scrollTop = $(window).scrollTop()
 
-    if($('header').height() <= scrollTop) {
+    if(0 < scrollTop) {
         $('header').css('box-shadow', '0 4px 4px -2px rgba(0,0,0,.2)')
         for(let elm of headerElms) {
           elm.addClass('header-is-animation')
